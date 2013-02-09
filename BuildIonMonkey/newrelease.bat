c8bf428a4b645bbed3a6e2ba8d53b3fa5793e687
Taliesin Sisson <taliesins@yahoo.com>
2013-02-09 19:35:03 +0000

    Copy library files

--- BuildIonMonkey/newrelease.bat
+++ BuildIonMonkey/newrelease.bat
@@ -15,6 +15,57 @@ if errorlevel 1 goto error
 cmd /c start-msvc11-x64
 if errorlevel 1 goto error
 
+set ionmonkeydir="C:\Data\ionmonkey"
+set reldir="Binaries"
+if exist %reldir% rmdir /s /q "%reldir%"
+mkdir "%reldir%"
+if errorlevel 1 goto error
+
+mkdir "%reldir%\lib"
+mkdir "%reldir%\lib\Net35"
+mkdir "%reldir%\lib\Net40"
+mkdir "%reldir%\lib\Net45"
+mkdir "%reldir%\lib\Net35\x86"
+mkdir "%reldir%\lib\Net35\amd64"
+mkdir "%reldir%\lib\Net40\x86"
+mkdir "%reldir%\lib\Net40\amd64"
+mkdir "%reldir%\lib\Net45\x86"
+mkdir "%reldir%\lib\Net45\amd64"
+
+mkdir "%reldir%\NativeBinaries"
+mkdir "%reldir%\NativeBinaries\Net35"
+mkdir "%reldir%\NativeBinaries\Net40"
+mkdir "%reldir%\NativeBinaries\Net45"
+mkdir "%reldir%\NativeBinaries\Net35\x86"
+mkdir "%reldir%\NativeBinaries\Net35\amd64"
+mkdir "%reldir%\NativeBinaries\Net40\x86"
+mkdir "%reldir%\NativeBinaries\Net40\amd64"
+mkdir "%reldir%\NativeBinaries\Net45\x86"
+mkdir "%reldir%\NativeBinaries\Net45\amd64"
+
+copy "%ionmonkeydir%\nsprpub\build-release-net35-amd64\dist\bin\libnspr4.dll" "%reldir%\NativeBinaries\Net35\amd64\"
+copy "%ionmonkeydir%\nsprpub\build-release-net35-x86\dist\bin\libnspr4.dll" "%reldir%\NativeBinaries\Net35\x86\"
+copy "%ionmonkeydir%\nsprpub\build-release-net40-amd64\dist\bin\libnspr4.dll" "%reldir%\NativeBinaries\Net40\amd64\"
+copy "%ionmonkeydir%\nsprpub\build-release-net40-x86\dist\bin\libnspr4.dll" "%reldir%\NativeBinaries\Net40\x86\"
+copy "%ionmonkeydir%\nsprpub\build-release-net45-amd64\dist\bin\libnspr4.dll" "%reldir%\NativeBinaries\Net45\amd64\"
+copy "%ionmonkeydir%\nsprpub\build-release-net45-x86\dist\bin\libnspr4.dll" "%reldir%\NativeBinaries\Net45\x86\"
+
+copy "%ionmonkeydir%\js\src\build-release-net35-amd64\dist\bin\mozjs.dll" "%reldir%\NativeBinaries\Net35\amd64\"
+copy "%ionmonkeydir%\js\src\build-release-net35-x86\dist\bin\mozjs.dll" "%reldir%\NativeBinaries\Net35\x86\"
+copy "%ionmonkeydir%\js\src\build-release-net40-amd64\dist\bin\mozjs.dll" "%reldir%\NativeBinaries\Net40\amd64\"
+copy "%ionmonkeydir%\js\src\build-release-net40-x86\dist\bin\mozjs.dll" "%reldir%\NativeBinaries\Net40\x86\"
+copy "%ionmonkeydir%\js\src\build-release-net45-amd64\dist\bin\mozjs.dll" "%reldir%\NativeBinaries\Net45\amd64\"
+copy "%ionmonkeydir%\js\src\build-release-net45-x86\dist\bin\mozjs.dll" "%reldir%\NativeBinaries\Net45\x86\"
+
+copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*" "%reldir%\NativeBinaries\Net35\x86\"
+copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\amd64\Microsoft.VC90.CRT\*.*" "%reldir%\NativeBinaries\Net35\amd64\"
+copy "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\redist\x86\Microsoft.VC100.CRT\*.*" "%reldir%\NativeBinaries\Net40\x86\"
+copy "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\redist\x64\Microsoft.VC100.CRT\*.*" "%reldir%\NativeBinaries\net40\amd64\"
+copy "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\redist\x86\Microsoft.VC110.CRT\*.*" "%reldir%\NativeBinaries\Net45\x86\"
+copy "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\redist\x64\Microsoft.VC110.CRT\*.*" "%reldir%\NativeBinaries\Net45\amd64\"
+
+
+
 goto end
 
 :error
